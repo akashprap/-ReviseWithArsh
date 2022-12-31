bea81ba03fe6c1ed117d4bab6f044dc51dd85fc7
akashprap <akashprap2002@gmail.com>
2022-12-31 15:32:57 +0530

    1st ques done

---
+++ Microsoft/Evaluate Reverse Polish Notation.py
@@ -0,0 +1,17 @@
+#Leetcode 150
+#Evaluate Reverse Polish Notation
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for x in tokens:
+            if x not in ["+","-","/","*"]:
+                stk.append(int(x))
+            else:
+                a = stk.pop(-1)
+                b = stk.pop(-1)
+                if x == "/":
+                    stk.append(int(b/a))
+                else:
+                    stk.append(eval(f"{b}{x}{a}"))
+
+        return stk[0]
