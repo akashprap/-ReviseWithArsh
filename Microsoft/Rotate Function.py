a448b3b2e44587f7ef924b6b7998e476d6f32216
akashprap <akashprap2002@gmail.com>
2023-01-02 15:54:36 +0530

    done

---
+++ Microsoft/Rotate Function.py
@@ -0,0 +1,11 @@
+#Leetcode 396
+#https://leetcode.com/problems/rotate-function/
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        n=len(nums)
+        s=sum(nums)
+        ans=F=sum([i*nums[i] for i in range(n)])
+        for i in range(1,n):
+            F=F+s-(n*nums[-i])
+            ans=max(ans,F)
+        return ans
